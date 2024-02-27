770940a37d35b687a3f9f7d32dc77b90107c4f2c
Nika Shilobod <69326376+stark1tty@users.noreply.github.com>
2024-02-27 18:24:14 +0000

    Update TimeSeries2Audio.R

--- TimeSeries2Audio.R
+++ TimeSeries2Audio.R
@@ -6,8 +6,6 @@ head(df)
 compressed <- (df$x - min(df$x)) / (max(df$x) - min(df$x))  # Now between 0 and 1
 compressed <- 2 * compressed - 1  # Rescale to be between -1 and 1
 
-print(min(compressed))
-
 span <- 0.5  # Span for the local regression, controls the degree of smoothing
 smoothed_x <- predict(loess(x ~ I(1:nrow(audiodata)), data=audiodata, span=span))
 print(smoothed_x)
